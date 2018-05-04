319f82deee5c9e9e34c07b96d02a8f715133dc58
Youngkyu <yk.hong@kaist.ac.kr>
2018-05-05 00:01:26 +0900

    divided body

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,68 @@
 import React, { Component } from 'react';
+import Title from './common/Title';
+import Note from './common/Note';
 
-class App extends Component {
+
+class Body extends Component {
   render() {
+    const titleComponents = data.map((e, i) =>
+      <Title title={e.title} index={i+1} />
+    );
+
     return (
-        <div className="App">
+      <div style={styles.backgroundStyle}>
+        <div style={styles.leftStyle}>
+          <div style={styles.paperStyle}>
+          </div>
         </div>
+        <div style={styles.rightStyle}>
+          {titleComponents}
+          <Note/>
+        </div>
+      </div>
     );
   }
 }
 
+const data = [
+  {
+    title: 'Introduction',
+  },
+  {
+    title: 'Deep Image Representation',
+  },
+  {
+    title: 'Result',
+  },
+  {
+    title: 'Discussion',
+  },
+];
+
 const styles = {
+  backgroundStyle: {
+    display: 'flex',
+    height: '100vh',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  leftStyle: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'blue',
+  },
+  rightStyle: {
+    width: '400px',
+    backgroundColor: 'red',
+  },
+  paperStyle: {
+    flex: 1,
+    maxWidth: '1000px',
+    backgroundColor: 'white',
+    height: '100%',
+    margin: 'auto',
+  }
+};
 
-}
-
-export default App;
+export default Body;

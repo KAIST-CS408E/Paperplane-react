79e542f2229444741930101611975d2eb65288ec
Youngkyu <yk.hong@kaist.ac.kr>
2018-05-12 16:36:01 +0900

    changed textarea into content editable div

--- src/components/common/Note.js
+++ src/components/common/Note.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Collapsible from 'react-collapsible';
 import './Note.css';
-import TextArea from 'react-textarea-autosize';
 import editIconPath from '../../icons/edit_icon.png';
+import TextArea from './TextArea';
 
 class Note extends Component {
   constructor(props) {
@@ -10,7 +10,8 @@ class Note extends Component {
     this.state = {
       mode: 'read',
       title: 'dd',
-      content: '',
+      content: <a>hi</a>,
+      html: '',
     };
   }
 
@@ -26,28 +27,39 @@ class Note extends Component {
     e.stopPropagation();
   }
 
+  getInitialState() {
+    return {html: "this is <em>an</em> <strong>example</strong>"};
+  }
+
+
   render() {
+    var handleChange = function(event){
+      this.setState({html: event.target.value});
+    }.bind(this);
+
     const trigger = this.state.mode === 'read' ?
         <div>{this.state.title} <img style={styles.iconStyle} src={editIconPath} onClick={(e) => this.changeTitleMode(e)} /></div> :
         (
-            <div onKeyPress={(e) => this.onEnterKeyPress(e)} onClick={(e) => e.stopPropagation()}>
-              <TextArea
-                  style={styles.titleTextArea}
-                  minRows={1}
-                  maxRows={1}
-                  placeholder={'Write title!'}
-                  onChange={event => this.setState({ title: event.target.value })}
-                  value={this.state.title} />
-            </div>
+          <div onKeyPress={(e) => this.onEnterKeyPress(e)} onClick={(e) => e.stopPropagation()}>
+            <TextArea
+              style={styles.titleTextArea}
+              minRows={1}
+              maxRows={1}
+              placeholder={'Write title!'}
+              onChange={event => this.setState({ title: event.target.value })}
+              value={this.state.title} />
+          </div>
         );
     return (
       <Collapsible trigger={trigger} transitionTime={100}>
-        <TextArea
-          style={styles.textArea}
-          minRows={4}
-          placeholder={'Write something meaningful to you!'}
-          onChange={event => this.setState({ content: event.target.value })}
-          value={this.state.content} />
+
+        {/*<TextArea*/}
+          {/*style={styles.textArea}*/}
+          {/*minRows={4}*/}
+          {/*placeholder={'Write something meaningful to you!'}*/}
+          {/*onChange={event => this.setState({ content: event.target.value })}*/}
+          {/*value={this.state.content} />*/}
+        <TextArea html={this.state.html} onChange={handleChange} />
       </Collapsible>
     );
   }

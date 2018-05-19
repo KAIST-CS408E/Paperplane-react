e3dcfff1ea1f69c35c6ff8e0d0c19d5c1305d8a1
Zeniuus <jsh56son@kaist.ac.kr>
2018-05-19 20:13:49 +0900

    Show 'No title' in summary page if note does not have title

--- src/components/common/SummaryNote.js
+++ src/components/common/SummaryNote.js
@@ -5,7 +5,7 @@ class SummaryNote extends Component {
   render() {
     return (
       <div style={styles.noteSummaryStyle}>
-        <h4 className="title is-5" style={styles.noteTitleStyle}>{this.props.note.title}</h4>
+        <h4 className="title is-5" style={styles.noteTitleStyle}>{this.props.note.title || 'No title'}</h4>
         <div dangerouslySetInnerHTML={{ __html: this.props.note.content }} />
       </div>
     );

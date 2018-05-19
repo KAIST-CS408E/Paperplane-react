24eca7d4e6d128519cfeaa5da8cfbe09514819b5
Zeniuus <jsh56son@kaist.ac.kr>
2018-05-19 22:03:28 +0900

    Apply basic CSS for SectionSummaryForm component

--- src/components/common/SectionSummaryForm.js
+++ src/components/common/SectionSummaryForm.js
@@ -2,13 +2,49 @@ import React, { Component } from 'react';
 
 
 class SectionSummaryForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isHidden: false,
+    };
+  }
+
+  hideForm = () => {
+    this.setState({ isHidden: true });
+  };
+
   render() {
     return (
-      <div>
-        <input placeholder="How about leaving a short summary for the previous section?" />
+      <div className="card" style={{ ...styles.sectionSummaryFormStyle, display: this.state.isHidden ? 'none' : 'block' }}>
+        <div className="card-content" style={styles.textAreaStyle}>
+          <textarea className="textarea" placeholder="How about leaving a short summary for the previous section?" />
+        </div>
+        <div style={styles.buttonGroupStyle}>
+          <button className="button is-success" style={styles.buttonStyle}>Submit!</button>
+          <button className="button" style={styles.buttonStyle} onClick={this.hideForm}>Hide</button>
+          <div style={{ clear: 'both' }} />
+        </div>
       </div>
     );
   }
 }
+
+const styles = {
+  sectionSummaryFormStyle: {
+    margin: '100px 0 200px 0',
+  },
+  textAreaStyle: {
+    padding: '15px',
+  },
+  buttonGroupStyle: {
+    display: 'block',
+    padding: '0 15px 15px',
+  },
+  buttonStyle: {
+    float: 'right',
+    marginLeft: '15px',
+  },
+};
 
 export default SectionSummaryForm;

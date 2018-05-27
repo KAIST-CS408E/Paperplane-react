2fddcd1b47cc2120305e81cbc0a83e8c8cab4d1c
Youngkyu <yk.hong@kaist.ac.kr>
2018-05-28 00:20:29 +0900

    removed section number

--- src/components/common/SummarySection.js
+++ src/components/common/SummarySection.js
@@ -76,7 +76,7 @@ class SummarySection extends Component {
     const sectionSummary = !summary ? null : <SummaryNote note={summary} contents={this.getContents(summary)} isSummary={true} />;
     return (
       <div style={styles.sectionSummaryContainerStyle}>
-        <h3 className="title is-3" style={styles.sectionTitleStyle}>{`${section.number}. ${section.name}`}</h3>
+        <h3 className="title is-3" style={styles.sectionTitleStyle}>{section.name}</h3>
         <div>
           {sectionNoteList}
           {sectionNoteList && sectionSummary ? <div style={{ height: '30px' }} /> : null }
